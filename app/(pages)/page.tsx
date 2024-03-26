92f8a42905d60f5f8588daa6e77d8577c7b0d390
Diego Corrales <dcorrales@brooksbell.com>
2024-03-26 15:12:44 -0700

    added fake server & updated data fetch

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,12 +1,6 @@
-import { ArticleApi } from '../api/article-api';
+import { ArticleApi } from '@/app/api/article-api';
 
 export default async function IndexPage() {
   const articles = await ArticleApi.fetchToday();
-  return (
-    <>
-      <h1 className='font-bold capitalize text-3xl'> Welcome to CLOS-IT </h1>
-      <div>A new app that works with Pinterest.</div>
-      {JSON.stringify(articles)}
-    </>
-  );
+  return <div>{JSON.stringify(articles)}</div>;
 }
--- app/api/article-api.ts
+++ app/api/article-api.ts
@@ -1,11 +1,11 @@
+import { Article, ArticleListResponse } from '../types/article-type';
+
 export class ArticleApi {
-  static async fetchToday(): Promise<any> {
-    const headers = {
-      Authorization: `Bearer ${process.env.API_KEY}`,
-      'Content-Type': 'application/json',
-    };
-    return await (
-      await fetch('https://api.pinterest.com/v5/boards', { headers })
-    ).json();
+  static async fetchToday(): Promise<Article[]> {
+    return (
+      (await (
+        await fetch('http://localhost:3090/news')
+      ).json()) as ArticleListResponse
+    ).results;
   }
 }
